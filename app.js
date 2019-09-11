52f3ad8c0891452f466e4fdf4faeee7e92f3e6d3
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-11 16:43:45 -0700

    create ui and app controller

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var budgetController = (function() {
   var x = 23;
 
-  var add = function (a) { // private add function
+  var add = function (a) {      // private add function
     return x + a;
   }
 
   return {
     publicTest: function(b) {
-      console.log(add(b));
+      return add(b);
     }
   }
 })();
 
+var UIController = (function(){
+
+
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {      // controls the app
+
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  }
+
+})(budgetController, UIController);
