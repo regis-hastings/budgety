8b9b3d85d6e8a2e7c972d7cf94ef7fdd3b5015fc
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-12 11:27:10 -0700

    add getInput and getDOMStrings method to UI controller

--- app.js
+++ app.js
@@ -7,17 +7,44 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function(){
+  
+  var DOMstrings = {                        // Create separate data structure
+    inputType: '.add__type',                // so that you can change strings
+    inputDescription: '.add__description',  // here and update the rest of the
+    inputValue: '.add__value',              // UI Controller at once
+    inputBtn: '.add__button'
+  }
+
+  return {
+
+    getInput: function() { // method for returning all 3 user inputs to UI
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
 
 
+  };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {      // controls the app
 
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
 
     // 1. Get field input data
+
+    var input = UICtrl.getInput();
+    console.log(input);
 
     // 2. Add item to the budget controller
 
@@ -27,17 +54,15 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
 
     // 5. Display budget on the UI
 
-    console.log('This works.');
-
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
   
   document.addEventListener('keypress', function(event) {
     if (event.keycode === 13 || event.which === 13) {
       ctrlAddItem();
     }
-    
+
   });
 
 })(budgetController, UIController);
