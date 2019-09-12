86674951e727a12e959ec90bd714fde0775d4fdd
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-12 15:47:39 -0700

    create income and expense constructors for budget controller

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ var budgetController = (function() {
     this.value = value;
   }
 
-})();
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
 
-var Expense = function(id, description, value) {
-  this.id = id;
-  this.description = description;
-  this.value = value;
-}
+})();
 
 // UI CONTROLLER
 var UIController = (function(){
@@ -90,5 +95,3 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
 })(budgetController, UIController);
 
 controller.init();
-
-// create income and expense constructors for budget controller
