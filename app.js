23aeb37a28a77c24e343805c67e09705c3f817f9
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-17 15:36:51 -0700

    finish displayBudget method for UI Controller

--- app.js
+++ app.js
@@ -105,7 +105,11 @@ var UIController = (function(){
     inputValue: '.add__value',              // UI Controller at once
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
 
   return {
@@ -162,6 +166,20 @@ var UIController = (function(){
 
     },
 
+    displayBudget: function(obj) {
+
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = `${obj.percentage} %`;
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -195,7 +213,7 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
     var budget = budgetCtrl.getBudget();
 
     // 3. Display budget on the UI
-    console.log(budget);
+    UIController.displayBudget(budget);
 
   }
 
@@ -225,6 +243,12 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
   return {
     init: function() {
       console.log('Application has started.');
+      UIController.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   }
