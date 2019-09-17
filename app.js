bda2eb7f4c5ecd68a2e2ac424a23ce6a3e31f5a7
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-17 12:25:43 -0700

    finish updateBudget funcion in controller and refactor calculateBudget method in budget controller

--- app.js
+++ app.js
@@ -73,8 +73,20 @@ var budgetController = (function() {
       data.budget = data.totals.inc - data.totals.exp;
 
       // calculate the percentage of income that we spent
-      data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+    },
 
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
     },
 
     testing: function() { // Really useful method for development. Not meant for production
@@ -180,8 +192,10 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
     budgetCtrl.calculateBudget();
 
     // 2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     // 3. Display budget on the UI
+    console.log(budget);
 
   }
 
