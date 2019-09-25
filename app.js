7c297d0139ddad931ea14acfff0bec75fbe2833b
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-25 14:00:52 -0700

    create displayPercentages method for UIController

--- app.js
+++ app.js
@@ -162,7 +162,8 @@ var UIController = (function(){
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage'
   }
 
   return {
@@ -239,6 +240,29 @@ var UIController = (function(){
 
     },
 
+    displayPercentages: function(percentages) {
+
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function(list, callback) {
+
+        for (var i = 0; i < list.length; i++) { // node list has length property
+          callback(list[i], i);
+        }
+
+      };
+
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+
+      });
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -285,7 +309,7 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
     var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
   }
 
   var ctrlAddItem = function() {
