f48075c46a1c22e51c0b9fb329bc113be6847005
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-09-28 11:16:34 -0700

    create changeType function

--- app.js
+++ app.js
@@ -195,6 +195,14 @@ var UIController = (function(){
     return `${type === 'exp' ? '-' : '+'} ${int}.${dec}`;
   };
 
+  var nodeListForEach = function(list, callback) {
+
+    for (var i = 0; i < list.length; i++) { // node list has length property
+      callback(list[i], i);
+    }
+
+  };
+
   return {
 
     getInput: function() { // method for returning all 3 user inputs to UI
@@ -279,14 +287,6 @@ var UIController = (function(){
 
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback) {
-
-        for (var i = 0; i < list.length; i++) { // node list has length property
-          callback(list[i], i);
-        }
-
-      };
-
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -313,6 +313,20 @@ var UIController = (function(){
 
     },
 
+    changedType: function() {
+
+      var fields = document.querySelectorAll( // returns a node list
+        `${DOMstrings.inputType},${DOMstrings.inputDescription},${DOMstrings.inputValue}`
+      );
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      })
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -336,6 +350,8 @@ var controller = (function(budgetCtrl, UICtrl) {      // controls the app
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   }
 
   var updateBudget = function() {
